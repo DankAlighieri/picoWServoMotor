460baa9f1f14d5ba51cb4dc9023c99f385aaca9d
Guilherme <guilhermeemeterioif@gmail.com>
2025-02-08 15:28:19 -0300

    Segunda rotina finalizada

--- servoMotorPWMWokwi.c
+++ servoMotorPWMWokwi.c
@@ -1,16 +1,3 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pwm.h"
-
-#define SERVO_PIN 28
-#define WRAP 65535
-#define CYCLE180 2.4
-#define CYCLE90 1.47
-#define CYCLE0 .5
-#define PWM_PERIOD 20
-#define CLK_DIV 38.147f
-
-
 // Calcular divisor de clock
 /*
     Frequência do PWM = 50Hz
@@ -20,15 +7,6 @@
     sys_clock = 125*10^6
     clock_div = 38,147
 */
-
-void setup_servo_pwm() {
-    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
-    uint slice = pwm_gpio_to_slice_num(SERVO_PIN);
-    pwm_config config = pwm_get_default_config();
-
-    pwm_config_set_clkdiv(&config, CLK_DIV);
-    pwm_init(slice, &config, true);
-}
 
 // Calcular duty cycle p/ cada ciclo ativo
 /*
@@ -43,31 +21,69 @@ void setup_servo_pwm() {
     duty_cycle 0 = (0,5/20) *(65335 + 1) =~ 1638
 */
 
-void initial_servo_routine(){
-    uint16_t duty_cycle180 = (uint16_t)((CYCLE180/PWM_PERIOD) * (WRAP + 1));
-    uint16_t duty_cycle90 = (uint16_t)((CYCLE90/PWM_PERIOD) * (WRAP + 1));
-    uint16_t duty_cycle0 = (uint16_t)((CYCLE0/PWM_PERIOD) * (WRAP + 1));
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
 
-    pwm_set_gpio_level(SERVO_PIN, duty_cycle180); // duty cycle 180
+#define SERVO_PIN 28
+#define WRAP 65535
+
+#define ACTIVE_CYCLE180 2.4
+#define ACTIVE_CYCLE90 1.47
+#define ACTIVE_CYCLE0 .5
+
+#define PWM_PERIOD 20
+#define CLK_DIV 38.147f
+
+#define DUTY_CYCLE_180 7864
+#define DUTY_CYCLE_90 4817
+#define DUTY_CYCLE_0 1638
+
+uint setup_servo_pwm() {
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(SERVO_PIN);
+    pwm_config config = pwm_get_default_config();
+
+    pwm_config_set_clkdiv(&config, CLK_DIV);
+    pwm_init(slice, &config, true);
+
+    return slice;
+}
+
+void initial_servo_routine(){
+    pwm_set_gpio_level(SERVO_PIN, DUTY_CYCLE_180);
     printf("Servo em 180 graus\n");
-    sleep_ms(5000);
+    sleep_ms(1000);
     printf("Servo em 90 graus\n");
-    pwm_set_gpio_level(SERVO_PIN, duty_cycle90); // duty cycle 90
-    sleep_ms(5000);
+    pwm_set_gpio_level(SERVO_PIN, DUTY_CYCLE_90);
+    sleep_ms(1000);
     printf("Servo em 0 graus\n");
-    pwm_set_gpio_level(SERVO_PIN, duty_cycle0); // duty cycle 0
-    sleep_ms(5000);
+    pwm_set_gpio_level(SERVO_PIN, DUTY_CYCLE_0);
+    sleep_ms(1000);
 }
 
 void periodic_servo_motion() {
-            
+    float active_cycle = ACTIVE_CYCLE0; // 500us
+    uint16_t duty_cycle;
+    bool full_range = false;
+
+    while (!full_range) {
+        active_cycle += 0.005; // Increment the active cycle by 5us
+        printf("Ciclo ativo: %.2f\n", active_cycle);
+        duty_cycle = (uint16_t)((active_cycle / PWM_PERIOD) * (WRAP + 1));
+        //printf("Duty cycle = %d\n", duty_cycle);
+        pwm_set_gpio_level(SERVO_PIN, duty_cycle);
+        if (active_cycle >= ACTIVE_CYCLE180) full_range = true;
+        sleep_ms(10);
+    }
 }
 
 int main() {
     stdio_init_all();
-    setup_pwm();
+    setup_servo_pwm();
 
     while (true) {
-        initial_servo_routine();
+        //initial_servo_routine();
+        periodic_servo_motion();
     }
 }
